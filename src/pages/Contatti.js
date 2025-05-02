d14a078240b0e058b7bce0d3becdcfaa57c03e10
Nicola Maraschi <nicola.maraschi01@gmail.com>
2025-05-02 23:20:14 +0200

    aggiornamenti imamgini in servizi

--- src/pages/Contatti.js
+++ src/pages/Contatti.js
@@ -101,12 +101,6 @@ const Contatti = () => {
                     <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
                       <i className="fab fa-instagram"></i>
                     </a>
-                    <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                      <i className="fab fa-linkedin-in"></i>
-                    </a>
-                    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                      <i className="fab fa-twitter"></i>
-                    </a>
                   </div>
                 </div>
                 
